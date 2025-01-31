bd301ea24697bd2392d729c2e9c316fe6c884a8f
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-02-01 04:11:12 +0700

    summ

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+        int x = 200; //начальная сумма на счету клиента
+        int y = 1300; //сумма пополнение сета
+        int bonus;
+
+
+        if (y > 1000) {
+            bonus = y / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(x + y + bonus); //итоговая сумма на счету клиента
+    }
+}
